049a28018f35cf44141f232596b1819bf24877cf
blazerpaul15 <blazerpaul15@gmail.com>
2026-04-26 12:18:37 +0900

    config: Build AxSandbox
    Signed-off-by: Zeyy <zeydann.sc@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -288,9 +288,6 @@ CUSTOM_LOCALES += \
     fur_IT \
     nn_NO
 
-PRODUCT_PACKAGES += \
-    OmniJaws
-
 # Face Unlock
 PRODUCT_PACKAGES += \
     ParanoidSense
@@ -300,6 +297,11 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+
+# Extra packages
+PRODUCT_PACKAGES += \
+    OmniJaws \
+    AxSandbox
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
